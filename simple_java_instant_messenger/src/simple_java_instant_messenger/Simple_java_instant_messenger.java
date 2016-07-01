2894d901747b25ef78959e0455a6584cb41e0b82
nialloc9@gmail.com <nialloc9@gmail.com>
2016-07-01 14:52:36 +0900

    Added Server Class

---
+++ simple_java_instant_messenger/src/simple_java_instant_messenger/Simple_java_instant_messenger.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package simple_java_instant_messenger;
+
+/**
+ *
+ * @author Niall
+ */
+public class Simple_java_instant_messenger {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
