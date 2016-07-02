acf4d62aa0305e7f445bc48797561643e7539a8c
nialloc9@gmail.com <nialloc9@gmail.com>
2016-07-02 15:33:12 +0900

    created showMessage and ableToEdit to client

--- simple_java_instant_messenger/src/client/Client.java
+++ simple_java_instant_messenger/src/client/Client.java
@@ -67,4 +67,68 @@ public class Client extends JFrame{
         input = new ObjectInputStream(connection.getInputStream()); //from server
         showMessage("\n Streams are connected. \n");
     }
+    
+    //while chatting with server
+    private void whileChatting() throws IOException{
+        ableToType(true);
+        
+        do{
+            try{
+                message = (String) input.readObject();
+                showMessage("\n "+ message);
+            }catch(ClassNotFoundException ex){
+                showMessage("\n don't know object type");
+            }
+        }while(!message.equals("SERVER - END")); //Do while message from server dosn't say SERVER - END
+    }
+    
+    //close all the streams and sockets
+    private void closeCrap(){
+        showMessage("\n closing everything down...");
+        ableToType(false);
+        
+        try{
+        input.close();
+        output.close();
+        connection.close();
+        }catch(IOException ex){
+            ex.printStackTrace();
+        }
+        
+    }
+    
+    //send messages to server
+    private void sendMessage(String message){
+        try{
+            output.writeObject("CLIENT - " + message); //Sends it 
+            output.flush();
+            showMessage("\n Client - " + message); //prints it to screen
+        }catch(IOException ex){
+            chatWindow.append("\n message send failure.");
+        }
+    }
+    
+    //update the GUI so message is shown in the GUI
+    private void showMessage(final String m){
+        SwingUtilities.invokeLater(
+                new Runnable() {
+            @Override
+            public void run() {
+                chatWindow.append(m);
+            }
+        }
+        );
+    }
+    
+    //allows user to type text into the textbox
+    private void ableToType(final boolean tof){
+        SwingUtilities.invokeLater(
+                new Runnable() {
+            @Override
+            public void run() {
+                userText.setEditable(tof);
+            }
+        }
+        );
+    }
 }
