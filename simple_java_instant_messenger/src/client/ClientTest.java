c7883186c9fad44e82fdec9474a9809e2ba79ef5
nialloc9@gmail.com <nialloc9@gmail.com>
2016-07-02 15:38:52 +0900

    Added ClientTest

--- simple_java_instant_messenger/src/client/ClientTest.java
+++ simple_java_instant_messenger/src/client/ClientTest.java
@@ -1,7 +1,12 @@
 package client;
 
+import javax.swing.JFrame;
+
 public class ClientTest {
     public static void main(String[] args){
-        
+        Client user;
+        user = new Client("127.0.0.1");
+        user.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        user.startRunning();
     }
 }
